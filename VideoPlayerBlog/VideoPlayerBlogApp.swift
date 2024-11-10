8cc540cbfdc5cce7c72a24e6002f31798b007068
anubhavTechrange <anubhav@techrange.co>
2024-11-10 23:44:28 +0530

    Initial Commit

---
+++ VideoPlayerBlog/VideoPlayerBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerBlogApp.swift
+//  VideoPlayerBlog
+//
+//  Created by Anubhav Rawat on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
